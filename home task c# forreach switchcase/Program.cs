218796a9a5c13a1c08c7be5411c3b0214173460e
ZiyaMemmedoglu <ziyamm@code.edu.az>
2024-10-23 12:41:20 +0400

    C# Intro-Reference-Value-Types

---
+++ home task c# forreach switchcase/Program.cs
@@ -0,0 +1,123 @@
+﻿#region
+//homm task 1:Verilmiş yazıdan içində boşluqlar olmayan yeni bir yazı düzəltmək 
+//string text = "Salam Dostlar";
+//foreach (char c in text) ;
+//text = "SalamDostlar";
+//Console.WriteLine(text);
+#endregion 1
+
+
+#region
+//home task 2:Verilmiş yazılar siyahısında verilmiş hərfdən neçə ədəd olduğunu tapan proqram
+//string[] name = { "baki", "qar", "armud", "nar" };
+//char a = 'a';
+//int count = 0;
+//foreach (string c in name)
+//    if (a == a)
+//    {
+//        count++;
+
+//    }
+//{
+//    Console.WriteLine(count);
+//}
+#endregion
+
+
+
+#region
+//home task 3:Verilmiş yazının əvvəlindəki boşluqlar silinmiş bir string düzəldən proqram
+// (Misalçün verilmiş yazı "   salam necesen?   "-dirsə yeni düzələcək string "salam necəsən?   " olmalıdır.
+//string text1 = "        Salam Baki";
+//string text2 = "salam baki";
+//foreach (var item in text2) ;
+//text1 = text2;
+
+//{
+//    Console.WriteLine(text1);
+//}
+#endregion
+
+#region
+//home task 4:Verilmiş 2 ədəd üzərində verilmiş simvola uyğun əməliyyat edən proqram. 
+//Simvol +,-,*və ya / olmalıdır, əks halda simvolu yenidən daxil edilməsi istənilməlidir.
+// (Misaçün simvol * olarsa verilmiş 2 ədəd vurulacaq və nəticəsi göstəriləcək)
+
+//string name = ("!");
+//int num1 = 8;
+//int num2 = 4;
+
+//switch (name)
+//{
+//    case "+":
+//        Console.WriteLine(num1 + num2);
+//        break;
+//    case "*":
+//        Console.WriteLine(num1 * num2);
+//        break;
+//    case "/":
+//        Console.WriteLine(num1 / num2);
+//        break;
+//    case "-":
+//        Console.WriteLine(num1 - num2);
+//        break;
+//    default:
+//        Console.WriteLine("yeniden daxil et");
+//        break;
+
+//}
+#endregion
+
+#region
+//home task 5:Verilmiş yazının içində A hərfinin olub olmadığını tapan proqram
+
+
+//string[] name = {"azad"};
+//char a = 'a';
+//foreach (string c in name) ;
+
+//if (a !=a)
+
+//    Console.WriteLine("a herfi movcuddur");
+
+//else
+//    Console.WriteLine("a herfi movcud deyil");
+
+#endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#region
+// home task 8:Verilmiş təhsil növü dəyərinə əsasən o təhsilin saatını göstərən proqram.
+//Əgər verilmiş dəyər "programming"dirsə console-da 400 saat
+//"design"dırsa 250 saat, 
+//"system"dirsə 200 saat,
+///heç biri deyilsə "təhsil novu yanlisdir" yazılmalıdır console-a.
+//string name = "user";
+//switch (name)
+//{
+//    case "programming":
+//        Console.WriteLine("400 saat");
+//        break;
+//    case "design":
+//        Console.WriteLine("250 saat");
+//        break;
+//    case "system":
+//        Console.WriteLine("200 saat");
+//        break;
+//    default:
+//        Console.WriteLine("tehsil novu yanlisdir");
+//        break;
+//}
+#endregion
